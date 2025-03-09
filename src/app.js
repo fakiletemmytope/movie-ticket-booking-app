afa3a40d6f7e71cd8b58ef71da158ad243d914ca
Fakile Temitope Oluwaseyi <fakiletemitope@gmail.com>
2025-03-09 13:38:03 +0100

    added path for uploading movie image

--- src/app.js
+++ src/app.js
@@ -5,10 +5,16 @@ import bodyParser from "body-parser";
 import { moviesRouter } from "./routes/movies.js";
 
 const app = express()
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//routes
 app.use('/api/users', userRouter)
 app.use('/auth', authRouter)
 app.use('/api/movies', moviesRouter)
+app.get('/', () => {
+    res.redirect('https://documenter.getpostman.com/view/16249004/2sAYdoFnqm')
+})
 export default app
